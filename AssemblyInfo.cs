ae1b6be66904b1647ae8f4ed31b6bf40928c32a1
Charlie Poole <charlie@nunit.com>
2009-07-04 22:50:06 -0700

    Add license file and file headers

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,4 +1,10 @@
-﻿using System.Reflection;
+﻿// *****************************************************
+// Copyright 2007-2009, Charlie Poole
+//
+// Licensed under the Open Software License version 3.0
+// *****************************************************
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
